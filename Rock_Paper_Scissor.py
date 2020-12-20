830df2b57b8b7210b52bd0db97f1498304c144c5
delfin mongendu <delfdevs@gmail.com>
2020-12-19 23:57:03 -0500

    refractor

--- Rock_Paper_Scissor.py
+++ Rock_Paper_Scissor.py
@@ -4,3 +4,23 @@ print("Scissors...")
 
 Player1 = input("Player 1, make your move  ")
 Player2 = input("Player 2, make your move  ")
+
+if player1 == player2:
+    print("It's a tie!")
+elif player1 == " ":
+    if palyer2 == " ":
+        print("")
+    elif player2 == " ":
+        print("")
+elif player1 == " ":
+    if palyer2 == " ":
+        print("")
+    elif player2 == " ":
+        print("")
+elif player1 == " ":
+    if palyer2 == " ":
+        print("")
+    elif player2 == " ":
+        print("")
+else:
+    print(" ")
