719fd568e797c278a7916ba4a9543f5fc62a3e1e
delfin mongendu <delfdevs@gmail.com>
2020-12-21 23:55:46 -0500

    computer

--- Rock_Paper_Scissor.py
+++ Rock_Paper_Scissor.py
@@ -4,17 +4,19 @@ print("Rock...")
 print("Paper...")
 print("Scissors...")
 
-print(random.randint(0,2))
-Player1 = input("Player 1, make your move  ")
-Player2 = input("Player 2, make your move  ")
 
-if player1 == player2:
-    print("It's a tie!")
-elif player1 == " ":
-    if palyer2 == " ":
-        print("")
-    elif player2 == " ":
-        print("")
+Player1 = input("Player 1, make your move  ")
+rand_num = random.randint(0,2)
+
+if rand_num  == 0:
+    computer = "rock"
+elif rand_num  == 1:
+    computer = "paper"
+else:
+    computer = "scissors"
+print(computer)
+
+
 elif player1 == " ":
     if palyer2 == " ":
         print("")
