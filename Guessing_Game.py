27a263402c99ca5da63126f9d46c4d6ca3345ac9
delfin mongendu <delfdevs@gmail.com>
2020-12-23 23:58:14 -0500

    while

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -1,3 +1,25 @@
 import random
 
-random_numbern= random.randint(1,10) # number 1 - 10
+random_number= random.randint(1,10) # number 1 - 10
+
+guess = input("Pikc a number from 1 to 10:  ")
+guess = int(guess)
+
+
+while guess != random_number
+    guess = input("Pick a number from 1 to 10:  ")
+    guess = int(guess)
+    
+    if guess < random_number:
+        print("TOO LOW")
+    elif guess > random_number:
+        
+
+if guess == random_number:
+    print("YOU GOT IT")
+elif guess < random_number:
+    print("TOO LOW!")
+else:
+    print("TOO HIGHT!")
+
+print(random_number)
