716b170fc2b5945a6d6f99fe495e9211fe849b24
delfin mongendu <delfdevs@gmail.com>
2020-12-24 02:32:24 -0500

    refactor

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -3,23 +3,25 @@ import random
 random_number= random.randint(1,10) # number 1 - 10
 
 guess = input("Pikc a number from 1 to 10:  ")
-guess = int(guess)
+guess = None
 
 
-while guess != random_number
+while True:
     guess = input("Pick a number from 1 to 10:  ")
     guess = int(guess)
     
     if guess < random_number:
         print("TOO LOW")
     elif guess > random_number:
-        
-
-if guess == random_number:
-    print("YOU GOT IT")
-elif guess < random_number:
-    print("TOO LOW!")
-else:
-    print("TOO HIGHT!")
+        print("TOO HIGH")
+    else:
+        print("YOU WON!!!!")
+        play_again = input("Do you want to play again? (y/n) ")
+        if play_again == "y":
+            random_number = random.randint(1,10)  # numbers 1 - 10
+            guess = None
+        else:
+            print("Thank you for playing!!")
+            break
 
 print(random_number)
