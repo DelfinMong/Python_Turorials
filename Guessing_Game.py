1e775d173544d14c61e4cadc1ca0c6e715d31312
delfin mongendu <delfdevs@gmail.com>
2020-12-25 23:51:49 -0500

    final

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -3,8 +3,6 @@ import random
 random_number= random.randint(1,10) # number 1 - 10
 
 guess = input("Pikc a number from 1 to 10:  ")
-guess = None
-
 
 while True:
     guess = input("Pick a number from 1 to 10:  ")
