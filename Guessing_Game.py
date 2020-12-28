61646e56e5234a059e472ecb94b7e1baf8fa9f99
delfin mongendu <delfdevs@gmail.com>
2020-12-27 21:08:32 -0500

    refactor

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -1,20 +1,22 @@
 import random
 
-random_number= random.randint(1,10) # number 1 - 10
+random_number = random.randint(1,10) # number 1 - 10  
 
-guess = input("Pikc a number from 1 to 10:  ")
+guess = input("Pick a number from 1 to 10:  ")  
 
 while True:
-    guess = input("Pick a number from 1 to 10:  ")
-    guess = int(guess)
+    guess = input("Pick a number from 1 to 10:  ")  
+    guess = int(guess) 
     
-    if guess < random_number:
+    if guess < random_number:  
         print("TOO LOW")
+        
     elif guess > random_number:
         print("TOO HIGH")
+        
     else:
         print("YOU WON!!!!")
-        play_again = input("Do you want to play again? (y/n) ")
+        play_again = input("Do you want to play again? (y/n) ") y - n
         if play_again == "y":
             random_number = random.randint(1,10)  # numbers 1 - 10
             guess = None
@@ -22,4 +24,4 @@ while True:
             print("Thank you for playing!!")
             break
 
-print(random_number)
+print(random_number)    # print()
--- Rock_Paper_Scissor.py
+++ Rock_Paper_Scissor.py
@@ -1,14 +1,22 @@
 from random import randint
+player_wins = 0
+computer_wins = 0
+
+while player_wins < 2 and computer_wins < 2:
+    print(f"Player_score: {player_wins} computer_score: {computer_wins}")
+    print("..rock..")
+    print("..paper..")
+    print("..scissors..")
 
 player = input("Player, make your move  ").lower()
-rand_num = randint(0,2)
-
-if rand_num  == 0:
+rand_num = randint(0,2) 
+if (rand_num  == 0):
     computer = "rock"
-elif rand_num  == 1:
+elif (rand_num  == 1):
     computer = "paper"
 else:
     computer = "scissors"
+    
 print(f"Computer plays {computer}")
 
 if player == computer:
