cd4baab32c6d40a506ba6886dcd24c0c6d495ed7
delfin mongendu <delfdevs@gmail.com>
2021-01-03 23:57:20 -0500

    tictactoe

---
+++ Tic_tac_toe.py
@@ -0,0 +1,12 @@
+
+row1 = [' ',' ',' ']
+row2 = [' ',' ',' ']
+row3 = [' ',' ',' ']
+row2[1] = 'x' 
+
+def display(row1,row2,row3):
+    print(row1)
+    print(row2)
+    print(row3)
+
+display(row1,row2,row3)
